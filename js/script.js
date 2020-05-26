d661851a8b05453d161b07996ebb3cb00ba39dbb
sav4yk <mail@sav4yk.ru>
2020-05-26 14:09:35 +0300

    Add layers, add rows in table

--- js/script.js
+++ js/script.js
@@ -1,6 +1,56 @@
 new Vue({
     el: '.app',
     data: {
-        message: 'Hi'
+        layers: [],
+        layersTypes: {
+            beet:{
+                priceOneSm: 25,
+                label: 'Свекла'
+            },
+            herring:{
+                priceOneSm: 25,
+                label: 'Сельдь'
+            },
+            carrot:{
+                priceOneSm: 25,
+                label: 'Морковь'
+            },
+            potato:{
+                priceOneSm: 25,
+                label: 'Картофель'
+            },
+            chickenEggYolk:{
+                priceOneSm: 25,
+                label: 'Яичный желток'
+            },
+            chickenEggGlair:{
+                priceOneSm: 25,
+                label: 'Яичный белок'
+            },
+            garlic:{
+                priceOneSm: 25,
+                label: 'Чеснок'
+            },
+            mayo:{
+                priceOneSm: 25,
+                label: 'Майонез'
+            }
+        },
+        defaultLayerType: 'potato',
+        defaultHeight: 5
+    },
+    computed: {
+
+    },
+    methods: {
+        addLayer(){
+            this.layers.push({
+                type: this.defaultLayerType,
+                height: this.defaultHeight
+            });
+        },
+        changeHeight(i, dh) {
+            this.layers[i].height += dh;
+        }
     }
 });
